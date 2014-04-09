689cfa1bfa38da539a025986b058e539a161b995
Christopher H. Casebeer <christopher@chc.name>
2014-04-08 22:37:03 -0400

    Add variable freq, custom output range to tone()
    Modify the tone() generator to accept either a fixed frequency, as
    before, or a generator producing frequencies. This permits time-varying
    frequncy output.
    
    Note that the signal's phase will be continuous across varying
    frequencies.
    
    Add min_ and max_ arguments to tone() to permit output ranges other than
    [-1, 1]. This is entirely equivalent to
    
        util.normalize(tone(frequency), -1, 1, min_, max_)
    
    and is added solely to make tone() more convenient to use as a "pulse"
    function, e.g. as an input to tone()'s own new frequency generator
    argument to create a pulsed frequency:
    
        tone(tone(5, 440, 880))

--- audiogen/generators.py
+++ audiogen/generators.py
@@ -1,7 +1,16 @@
+# coding=utf8
+
+'''
+Assorted generators 
+'''
+
+import math
+import itertools
 
 import util
-import math
 import sampler
+
+TWO_PI = 2 * math.pi
 
 ## Audio sample generators
 
@@ -9,14 +18,32 @@ def beep(frequency=440):
 	for sample in util.crop_with_fades(tone(440), seconds=0.25):
 		yield sample
 
-def tone(frequency=440):
-	# todo limit length in seconds or cycles
-	period = int(sampler.FRAME_RATE / frequency) 
-	time_scale = 2 * math.pi / period # period * scale = 2 * pi
-	samples = [math.sin(i * time_scale) for i in xrange(period)]
-	while True:
-		for i in xrange(period):
-			yield samples[i]
+def tone(frequency=440, min_=-1, max_=1):
+	def fixed_tone(frequency):
+		period = int(sampler.FRAME_RATE / frequency) 
+		time_scale = 2 * math.pi / period # period * scale = 2 * pi
+		# precompute fixed tone samples # TODO: what about phase glitches at end?
+		samples = [math.sin(i * time_scale) for i in xrange(period)]
+		while True:
+			for i in xrange(period):
+				yield samples[i]
+	def variable_tone(frequency):
+		time_scale = TWO_PI / sampler.FRAME_RATE
+		phase = 0
+		for f in frequency:
+			yield math.sin(phase)
+
+			phase += time_scale * f
+
+			# don't reset hard to zero – avoids sudden phase glitches due to rounding error
+			if phase > TWO_PI:
+				phase -= TWO_PI
+
+	if not hasattr(frequency, 'next'):
+		gen = fixed_tone(frequency)
+	else:
+		gen = variable_tone(frequency)
+	return util.normalize(gen, -1, 1, min_, max_)
 
 def synth(freq, angles):
 	if isinstance(angles, (int, float)):
