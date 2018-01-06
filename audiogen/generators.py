b38a5e30105c74b3e72370f42d8c95cbc2ee4581
Christopher H. Casebeer <christopher@chc.name>
2018-01-05 20:36:34 -0500

    Add phase_offset arg to tone generator
    Allow specifying a phase offset for the tone generator, either advancing
    or delaying the phase of the generated sine wave.
    
    Useful for e.g. quadrature signals.

--- audiogen/generators.py
+++ audiogen/generators.py
@@ -18,18 +18,28 @@ def beep(frequency=440, seconds=0.25):
 	for sample in util.crop_with_fades(tone(frequency), seconds=seconds):
 		yield sample
 
-def tone(frequency=440, min_=-1, max_=1):
+def tone(frequency=440, phase_offset=0, min_=-1, max_=1):
 	def fixed_tone(frequency):
 		period = int(sampler.FRAME_RATE / frequency) 
 		time_scale = 2 * math.pi / period # period * scale = 2 * pi
 		# precompute fixed tone samples # TODO: what about phase glitches at end?
-		samples = [math.sin(i * time_scale) for i in xrange(period)]
-		while True:
+
+		def phase_indexes():
+			phase_index_offset = int(phase_offset / time_scale)
 			for i in xrange(period):
-				yield samples[i]
+				phase_index = i + phase_index_offset
+				if phase_index >= period:
+					phase_index -= period
+				elif phase_index < 0:
+					phase_index += period
+				yield phase_index
+		
+		samples = [math.sin(i * time_scale) for i in phase_indexes()]
+		return itertools.cycle(samples)
+
 	def variable_tone(frequency):
 		time_scale = TWO_PI / sampler.FRAME_RATE
-		phase = 0
+		phase = phase_offset
 		for f in frequency:
 			yield math.sin(phase)
 
@@ -38,6 +48,8 @@ def tone(frequency=440, min_=-1, max_=1):
 			# don't reset hard to zero – avoids sudden phase glitches due to rounding error
 			if phase > TWO_PI:
 				phase -= TWO_PI
+			elif phase < 0:
+				phase += TWO_PI
 
 	if not hasattr(frequency, 'next'):
 		gen = fixed_tone(frequency)
