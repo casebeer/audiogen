1796faba7f8ac3a2ae0a17942d26a86b089c0faa
Christopher H. Casebeer <christopher@chc.name>
2024-04-17 03:22:33 -0400

    Add DDS implementation in lieu of existing tone() code
    The tone() code had accumulating frequency errors. Properly implementing
    direct digital synthesis solves this.
    
    Additionally, rather than using the "fade" util functions to soften the
    edges of beeps, use a bandpass filter.

--- audiogen/generators.py
+++ audiogen/generators.py
@@ -4,58 +4,156 @@
 Assorted generators 
 '''
 
+import logging
+logger = logging.getLogger(__name__)
+
 import math
 import itertools
 
 import util
 import sampler
+import filters
 
 TWO_PI = 2 * math.pi
 
 ## Audio sample generators
 
-def beep(frequency=440, seconds=0.25):
-	for sample in util.crop_with_fades(tone(frequency), seconds=seconds):
-		yield sample
+bpf_cache = {}
+def beep(frequency=440, seconds=0.25, bpf=True):
+    if bpf:
+        if frequency not in bpf_cache:
+            bandwidth = max(frequency // 2 ** 6, 128)
+            logger.debug('Creating {} Hz BPF centered at {} Hz'.format(bandwidth, frequency))
+            bpf_cache[frequency] = filters.band_pass(frequency, bandwidth)
+        # lower volume 0.25 dB (~95% full scale) so BPF ripple doesn't exceed 0 dbFS
+        for sample in bpf_cache[frequency](itertools.chain(
+            util.crop(util.volume(tone(frequency), -0.25), seconds=(seconds - 0.01)),
+            silence(0.01)
+            )):
+            yield sample
+    else:
+        #for sample in util.crop_with_fades(tone(frequency), seconds=seconds):
+        for sample in util.crop(tone(frequency), seconds=seconds):
+            yield sample
 
-def tone(frequency=440, phase_offset=0, min_=-1, max_=1):
-	def fixed_tone(frequency):
-		period = int(sampler.FRAME_RATE / frequency) 
-		time_scale = 2 * math.pi / period # period * scale = 2 * pi
-		# precompute fixed tone samples # TODO: what about phase glitches at end?
 
-		def phase_indexes():
-			phase_index_offset = int(phase_offset / time_scale)
-			for i in xrange(period):
-				phase_index = i + phase_index_offset
-				if phase_index >= period:
-					phase_index -= period
-				elif phase_index < 0:
-					phase_index += period
-				yield phase_index
-		
-		samples = [math.sin(i * time_scale) for i in phase_indexes()]
-		return itertools.cycle(samples)
+class DDS(object):
+    accumulatorBits = 24
+    lutBits = 14
 
-	def variable_tone(frequency):
-		time_scale = TWO_PI / sampler.FRAME_RATE
-		phase = phase_offset
-		for f in frequency:
-			yield math.sin(phase)
+    # lut will be generated once on first use of DDS.dds() or by calling DDS.generateLut() directly
+    lut = None
 
-			phase += time_scale * f
+    @classmethod
+    def generateLut(cls):
+        import time
+        import sys
+        phasePerLutItem = 2 * math.pi / 2 ** cls.lutBits
+        start = time.time()
+        cls.lut = [ math.sin(i * phasePerLutItem) for i in range(2 ** cls.lutBits) ]
+        generationTime = time.time() - start
+        logger.debug('[dds] Generated DDS LUT in {} seconds'.format(generationTime))
+        logger.debug('[dds] {} bit phase accumulator, {} bit DDS LUT'.format(
+            cls.accumulatorBits,
+            cls.lutBits,
+        ))
+        logger.debug('[dds] {} bit ({:,} entry) LUT takes {:,} bytes of memory ({} bits / entry)'.format(
+            cls.lutBits,
+            2 ** cls.lutBits,
+            sys.getsizeof(cls.lut),
+            sys.getsizeof(cls.lut) / 2 ** cls.lutBits * 8,
+        ))
 
-			# don't reset hard to zero – avoids sudden phase glitches due to rounding error
-			if phase > TWO_PI:
-				phase -= TWO_PI
-			elif phase < 0:
-				phase += TWO_PI
+    @classmethod
+    def dds(cls, freqHz, phaseOffsetRad=0):
+        # https://www.allaboutcircuits.com/technical-articles/basics-of-phase-truncation-in-direct-digital-synthesizers/
+        if cls.lut is None:
+            cls.generateLut()
+        accumulatorSize = 2 ** cls.accumulatorBits
 
-	if not hasattr(frequency, 'next'):
-		gen = fixed_tone(frequency)
-	else:
-		gen = variable_tone(frequency)
-	return util.normalize(gen, -1, 1, min_, max_)
+        freqNorm = float(freqHz) / sampler.FRAME_RATE # 1 / samples
+        deltaPhase = int(round(freqNorm * accumulatorSize)) # accumulatorPhases / sample
+
+        logger.debug('[dds][acc] frequency resolution ≈ {} Hz'.format(
+            round(float(sampler.FRAME_RATE) / accumulatorSize, 5)))
+        logger.debug('[dds][acc] phase accumulator will overflow every {} seconds ({} Hz)'.format(
+            round(float(accumulatorSize) / deltaPhase / sampler.FRAME_RATE, 5),
+            round(float(deltaPhase) / accumulatorSize * sampler.FRAME_RATE, 5),
+            ))
+        logger.debug('[dds][lut] phase truncation spurs ~ {} dB below fundamental'.format(6.08 * cls.lutBits))
+
+        phaseAccumulator = int(0 + phaseOffsetRad / freqNorm) % accumulatorSize
+        truncateBits = cls.accumulatorBits - cls.lutBits
+        while True:
+            phaseAccumulator += deltaPhase
+            if phaseAccumulator >= accumulatorSize:
+                phaseAccumulator -= accumulatorSize
+            yield cls.lut[phaseAccumulator >> truncateBits]
+
+def dds(freqHz=440, phaseOffsetRad=0):
+    return DDS.dds(freqHz, phaseOffsetRad)
+    
+def tone(frequency=440, phase_offset=0, min_=-1, max_=1, frame_rate=None):
+    return DDS.dds(frequency)
+
+    if frame_rate is None:
+        frame_rate = sampler.FRAME_RATE
+    def fixed_tone(frequency):
+        frequencyHz = float(frequency)
+        periodf = float(frame_rate) / frequency
+        period = int(frame_rate / frequency)
+        time_scale = 2 * math.pi / periodf # period * scale = 2 * pi
+        # precompute fixed tone samples # TODO: what about phase glitches at end?
+
+        logger.debug('Periodf is {}'.format(periodf))
+
+        def phase_indexes():
+            phase_index_offset = int(phase_offset / time_scale)
+            for i in xrange(period * 1):
+                phase_index = i + phase_index_offset
+                if phase_index >= period:
+                    phase_index -= period
+                elif phase_index < 0:
+                    phase_index += period
+                yield phase_index
+        
+        #frequency                        # 1 / seconds
+        freqNorm = frequencyHz / frame_rate # 1 / samples
+        period = 1. / frequency           # seconds
+        periodNorm = 1. / freqNorm        # samples
+
+        # every periodNorm samples we want sin(sample) to cycle; 
+        # i.e. sin(periodNorm * cyclesPerSample) = sin(2 * PI)
+
+        cyclesPerSample = 2 * math.pi / periodNorm # 1 / samples
+
+        samples = [math.sin(i * cyclesPerSample) for i in range(int(periodNorm * 2))]
+        
+        #samples = [math.sin(i * time_scale) for i in phase_indexes()]
+        #samples = [math.sin(i * time_scale) for i in range(int(periodf * 1))]
+        #samples = [math.sin(i * samples_per_second) for i in range(int(periodf * 1))]
+
+        return itertools.cycle(samples)
+
+    def variable_tone(frequency):
+        time_scale = TWO_PI / frame_rate
+        phase = phase_offset
+        for f in frequency:
+            yield math.sin(phase)
+
+            phase += time_scale * f
+
+            # don't reset hard to zero – avoids sudden phase glitches due to rounding error
+            if phase > TWO_PI:
+                phase -= TWO_PI
+            elif phase < 0:
+                phase += TWO_PI
+
+    if not hasattr(frequency, 'next'):
+        gen = fixed_tone(frequency)
+    else:
+        gen = variable_tone(frequency)
+    return util.normalize(gen, -1, 1, min_, max_)
 
 def synth(freq, angles):
 	if isinstance(angles, (int, float)):
