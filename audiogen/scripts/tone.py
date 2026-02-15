da38cd66b4de2c769776fa102250f8f0f86caccd
Christopher H. Casebeer <christopher@chc.name>
2026-02-15 18:53:56 -0500

    Linter cleanups of tone.py

--- audiogen/scripts/tone.py
+++ audiogen/scripts/tone.py
@@ -1,28 +1,31 @@
 # coding=utf8
 
 import logging
-logger = logging.getLogger(__name__)
+import argparse
 
-import sys
 import audiogen
 
-import argparse
+logger = logging.getLogger(__name__)
 
 
 def main():
     parser = argparse.ArgumentParser(
             formatter_class=argparse.ArgumentDefaultsHelpFormatter
         )
-    parser.add_argument("-o", "--output-file",
+    parser.add_argument(
+        "-o", "--output-file",
         default=None,
-        help="Output file to write. If not set, tone audio plays with no file output."
+        help="Output file to write."
+             "If not set, tone audio plays without writing file."
     )
-    parser.add_argument("-s", "--seconds",
+    parser.add_argument(
+        "-s", "--seconds",
         type=float,
         default=2.0,
         help="Length of audio to play in seconds. Partial seconds allowed."
     )
-    parser.add_argument("frequency", nargs='?', default=440, type=int,
+    parser.add_argument(
+        "frequency", nargs='?', default=440, type=int,
         help="Frequency of tone to play."
         )
     args = parser.parse_args()
@@ -36,6 +39,7 @@ def main():
         logger.info('Writing {}...'.format(filename))
         with open(filename, 'wb') as f:
             audiogen.sampler.write_wav(f, tone)
+
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
